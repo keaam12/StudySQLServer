49bc57ee377e39d4431004624c17c6d32c1212ea
홍건의 <keaam12@google.com>
2021-05-12 17:50:06 +0900

    210512

---
+++ Sample/210512 최초연습.sql
@@ -0,0 +1,7 @@
+SELECT memberID, memberName
+	FROM memberTBL
+	WHERE memberID = 'Sung'
+-- SELECT productName, cost, company  FROM productTBL;
+
+INSERT INTO memberTBL
+VALUES	 ('Bang', '规巩侥', '何魂矫 模备'); 
