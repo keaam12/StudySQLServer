c686cc79052a33e69d9e129a882fd0ea66199da1
홍건의 <keaam12@google.com>
2021-05-13 10:39:11 +0900

    210513 1!!

---
+++ 210513/SQLQuery1.sql
@@ -0,0 +1,20 @@
+-- 데이터 조회
+SELECT * FROM userTbl;
+
+--데이터 조회 필터링
+SELECT * FROM userTbl
+ WHERE userID = 'BBK';
+
+SELECT * FROM userTbl
+ where NAME LIKE '%용%';
+
+ --특정 테이블 레코드 갯수 확인
+ SELECT COUNT(userID) FROM userTbl;
+
+ -- 사용자 테이블에서 
+ -- 키가 180 이상이고 출생년도가 1970 이후에 태어난 사람
+ -- 의 아이디, 이름, 키를 조회하세요
+ SELECT userID, name, height FROM userTbl
+  WHERE height >= 180
+    AND birthYear >= 1970;
+
